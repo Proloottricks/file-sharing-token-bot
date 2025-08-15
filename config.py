e355f5ade097ee976fc960c967033edb60200fa1
Proloottricks <ritiksakhala17@gmail.com>
2025-08-15 14:32:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "Sanskari_Links69") # WITHOUR @
 CHANNEL = os.environ.get("CHANNEL", "Sanskari_Links1") # WITHOUR @
 
 #Shortner (token system) 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "genzurl.com")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "iskerts.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "ce56b38a3dde9def1a225ac283da3fded9dcf848")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 64800)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
