92bb21e74cb6e7fc5e417bcba774d3cb6c1e9289
Proloottricks <ritiksakhala17@gmail.com>
2025-09-20 13:44:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,8 +80,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n {uptime}"
 USER_REPLY_TEXT = (
-    "Join Posting Channel From https://t.me/+H5JxDV-2fJwxNTY9 For More Links/Files"
-)
+    "Join Posting Channel From https://t.me/+H5JxDV-2fJwxNTY9 For More Links/Files")
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6376864232)
