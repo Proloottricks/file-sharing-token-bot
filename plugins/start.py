f63997f2f78edccc5fe20359de63d1cd65d8fe4f
Proloottricks <ritiksakhala17@gmail.com>
2026-04-13 15:05:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -99,7 +99,7 @@ async def start_command(client: Client, message: Message):
             await update_verify_status(id, is_verified=True, verified_time=time.time())
 
             btn = [
-                [InlineKeyboardButton("👉 Click here for Video Links", url="https://t.me/+sMwuj9ladUhhYTJl")]
+                [InlineKeyboardButton("👉 Click here for Video Links", url="https://t.me/+eFlQ5EUadj0xM2Ex")]
             ]
 
             await message.reply(
